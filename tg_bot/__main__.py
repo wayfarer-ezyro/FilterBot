0398e4f81069e12bf0c3e982e26e4b6b9c63578b
wayfarer-ezyro <72940489+wayfarer-ezyro@users.noreply.github.com>
2021-06-15 20:11:36 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -19,7 +19,7 @@ from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
 
-Hello {},My Name is {} !. 
+Hello {},/n/nI’m {}!. 
 
 I'm Filter Manager Bot Maintained By [{}](https://t.me/{}). 
 
